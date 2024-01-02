5bc6e1ead19e78f9426611d4375294e4cbb931d0
Ricardo Lima Caratti <pu2clr@gmail.com>
2024-01-01 21:40:33 -0300

    Documentation

--- projects/03-PIC16F_LCD16x2/lcd_library/MPLAB_PROJECTS/PIC16F887_LCD.X/nbproject/Makefile-default.mk
+++ projects/03-PIC16F_LCD16x2/lcd_library/MPLAB_PROJECTS/PIC16F887_LCD.X/nbproject/Makefile-default.mk
@@ -51,17 +51,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=newmain1.c
+SOURCEFILES_QUOTED_IF_SPACED=newmain1.c ../../pic16flcd.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/newmain1.p1
-POSSIBLE_DEPFILES=${OBJECTDIR}/newmain1.p1.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/newmain1.p1 ${OBJECTDIR}/_ext/43898991/pic16flcd.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/newmain1.p1.d ${OBJECTDIR}/_ext/43898991/pic16flcd.p1.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/newmain1.p1
+OBJECTFILES=${OBJECTDIR}/newmain1.p1 ${OBJECTDIR}/_ext/43898991/pic16flcd.p1
 
 # Source Files
-SOURCEFILES=newmain1.c
+SOURCEFILES=newmain1.c ../../pic16flcd.c
 
 
 
@@ -92,18 +92,34 @@ ${OBJECTDIR}/newmain1.p1: newmain1.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/newmain1.p1.d 
 	@${RM} ${OBJECTDIR}/newmain1.p1 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1  -mdebugger=none   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall -mno-default-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/newmain1.p1 newmain1.c 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1  -mdebugger=pickit3   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -Os -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall -mno-default-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/newmain1.p1 newmain1.c 
 	@-${MV} ${OBJECTDIR}/newmain1.d ${OBJECTDIR}/newmain1.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/newmain1.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/_ext/43898991/pic16flcd.p1: ../../pic16flcd.c  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}/_ext/43898991" 
+	@${RM} ${OBJECTDIR}/_ext/43898991/pic16flcd.p1.d 
+	@${RM} ${OBJECTDIR}/_ext/43898991/pic16flcd.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1  -mdebugger=pickit3   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -Os -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall -mno-default-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/_ext/43898991/pic16flcd.p1 ../../pic16flcd.c 
+	@-${MV} ${OBJECTDIR}/_ext/43898991/pic16flcd.d ${OBJECTDIR}/_ext/43898991/pic16flcd.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/_ext/43898991/pic16flcd.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 else
 ${OBJECTDIR}/newmain1.p1: newmain1.c  nbproject/Makefile-${CND_CONF}.mk 
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/newmain1.p1.d 
 	@${RM} ${OBJECTDIR}/newmain1.p1 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall -mno-default-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/newmain1.p1 newmain1.c 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -Os -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall -mno-default-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/newmain1.p1 newmain1.c 
 	@-${MV} ${OBJECTDIR}/newmain1.d ${OBJECTDIR}/newmain1.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/newmain1.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/_ext/43898991/pic16flcd.p1: ../../pic16flcd.c  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}/_ext/43898991" 
+	@${RM} ${OBJECTDIR}/_ext/43898991/pic16flcd.p1.d 
+	@${RM} ${OBJECTDIR}/_ext/43898991/pic16flcd.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -Os -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall -mno-default-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/_ext/43898991/pic16flcd.p1 ../../pic16flcd.c 
+	@-${MV} ${OBJECTDIR}/_ext/43898991/pic16flcd.d ${OBJECTDIR}/_ext/43898991/pic16flcd.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/_ext/43898991/pic16flcd.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 endif
 
@@ -124,13 +140,13 @@ endif
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 ${DISTDIR}/PIC16F887_LCD.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    
 	@${MKDIR} ${DISTDIR} 
-	${MP_CC} $(MP_EXTRA_LD_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -Wl,-Map=${DISTDIR}/PIC16F887_LCD.X.${IMAGE_TYPE}.map  -D__DEBUG=1  -mdebugger=none  -DXPRJ_default=$(CND_CONF)  -Wl,--defsym=__MPLAB_BUILD=1   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall -mno-default-config-bits -std=c99 -gdwarf-3 -mstack=compiled:auto:auto        $(COMPARISON_BUILD) -Wl,--memorysummary,${DISTDIR}/memoryfile.xml -o ${DISTDIR}/PIC16F887_LCD.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}     
+	${MP_CC} $(MP_EXTRA_LD_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -Wl,-Map=${DISTDIR}/PIC16F887_LCD.X.${IMAGE_TYPE}.map  -D__DEBUG=1  -mdebugger=pickit3  -DXPRJ_default=$(CND_CONF)  -Wl,--defsym=__MPLAB_BUILD=1   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -Os -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall -mno-default-config-bits -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -mrom=default,-1f00-1ffe -mram=default,-0-0,-70-70,-80-80,-f0-f0,-100-100,-170-170,-180-180,-1e5-1f0  $(COMPARISON_BUILD) -Wl,--memorysummary,${DISTDIR}/memoryfile.xml -o ${DISTDIR}/PIC16F887_LCD.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}     
 	@${RM} ${DISTDIR}/PIC16F887_LCD.X.${IMAGE_TYPE}.hex 
 	
 else
 ${DISTDIR}/PIC16F887_LCD.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk   
 	@${MKDIR} ${DISTDIR} 
-	${MP_CC} $(MP_EXTRA_LD_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -Wl,-Map=${DISTDIR}/PIC16F887_LCD.X.${IMAGE_TYPE}.map  -DXPRJ_default=$(CND_CONF)  -Wl,--defsym=__MPLAB_BUILD=1   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall -mno-default-config-bits -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     $(COMPARISON_BUILD) -Wl,--memorysummary,${DISTDIR}/memoryfile.xml -o ${DISTDIR}/PIC16F887_LCD.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}     
+	${MP_CC} $(MP_EXTRA_LD_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -Wl,-Map=${DISTDIR}/PIC16F887_LCD.X.${IMAGE_TYPE}.map  -DXPRJ_default=$(CND_CONF)  -Wl,--defsym=__MPLAB_BUILD=1   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -Os -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall -mno-default-config-bits -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     $(COMPARISON_BUILD) -Wl,--memorysummary,${DISTDIR}/memoryfile.xml -o ${DISTDIR}/PIC16F887_LCD.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}     
 	
 endif
 
--- projects/03-PIC16F_LCD16x2/lcd_library/pic16f887_example.c
+++ projects/03-PIC16F_LCD16x2/lcd_library/pic16f887_example.c
@@ -1,5 +1,5 @@
 #include <xc.h>
-#include "pic16flcd.h"
+#include "../../pic16flcd.h"
 
 #pragma config FOSC = INTRC_NOCLKOUT
 #pragma config WDTE = OFF       // Watchdog Timer disabled 
@@ -12,7 +12,38 @@
 
 #define _XTAL_FREQ 4000000      // internal clock
 
+/**
+ Custom Char (Smile / Happy face)
+ */
+unsigned char smiley[8] = {
+    0b00000,
+    0b01010,
+    0b01010,
+    0b00000,
+    0b10001,
+    0b01110,
+    0b00000,
+    0b00000
+};
+
+/**
+ Custom char (sad face)
+ */
+unsigned char sad[8] = {
+    0b00000,
+    0b01010,
+    0b01010,
+    0b00000,
+    0b00000,
+    0b01110,
+    0b10001,
+    0b00000
+};
+
+
+
 void main() {
+    unsigned char i;
     // Define the LCD pin configuration for PIC16F887
     TRISC = 0; // You need to set this register as output
     Lcd_PinConfig lcd = {
@@ -35,7 +66,33 @@ void main() {
     Lcd_SetCursor(&lcd, 2, 1);
     Lcd_WriteString(&lcd, "World");
 
+        // Creating the character
+    Lcd_CreateCustomChar(&lcd, 0, smiley);
+    Lcd_CreateCustomChar(&lcd, 1, sad);    
+    __delay_ms(3000);
     while(1) {
-        // Main loop
+        Lcd_Clear(&lcd);
+        for (i = 1; i <= 16; i++) {
+            Lcd_SetCursor(&lcd, 1, i);
+            Lcd_WriteChar(&lcd, 'A' + (i - 1));
+            __delay_ms(150);
+        }
+        __delay_ms(2000);
+
+        for (i = 1; i <= 16; i++) {
+            Lcd_SetCursor(&lcd, 2, i);
+            Lcd_WriteChar(&lcd, 'Q' + (i - 1));
+            __delay_ms(150);
+        }
+        __delay_ms(2000);
+        for (i = 1; i <= 16; i++) {
+            Lcd_SetCursor(&lcd, 1, i);
+            Lcd_WriteCustomChar(&lcd, 0); // Happy face
+            __delay_ms(150);
+            Lcd_SetCursor(&lcd, 2, i);
+            Lcd_WriteCustomChar(&lcd, 1); // Sad face
+            __delay_ms(200);
+        }
+        __delay_ms(2000);
     }
 }
