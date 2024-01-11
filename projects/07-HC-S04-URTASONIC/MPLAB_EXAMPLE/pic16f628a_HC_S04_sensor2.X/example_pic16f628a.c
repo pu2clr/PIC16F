7c38347c3f395c4e34c09ee61cc7bea6619e0a3f
Ricardo Lima Caratti <pu2clr@gmail.com>
2024-01-10 22:13:02 -0300

    Documentation

---
+++ projects/07-HC-S04-URTASONIC/MPLAB_EXAMPLE/pic16f628a_HC_S04_sensor2.X/example_pic16f628a.c
@@ -0,0 +1,79 @@
+#include <xc.h>
+
+// Chip settings
+#pragma config FOSC = INTOSCCLK // Internal oscillator, CLKOUT on RA6
+#pragma config WDTE = OFF       // Disables Watchdog Timer
+#pragma config PWRTE = OFF      // Disables Power-up Timer
+#pragma config MCLRE = ON       // MCLR pin function is digital input
+#pragma config BOREN = ON       // Enables Brown-out Reset
+#pragma config LVP = OFF        // Low voltage programming disabled
+#pragma config CPD = OFF        // Data EE memory code protection disabled
+#pragma config CP = OFF         // Flash program memory code protection disabled
+
+#define _XTAL_FREQ 4000000 // Internal oscillator frequency set to 4MHz
+
+
+void inline set_red_light() {
+    PORTB = 0b00000001;
+}
+
+void inline set_green_light() {
+    PORTB = 0b00000100;
+}
+
+void inline set_yellow_light() {
+    PORTB = 0b00000010;
+}
+
+void inline turn_led_off() {
+    PORTB = 0b00000000;
+}
+
+void main() {
+    
+    // Turns off the comparator and converts its pins in digital I/O. 
+    CMCONbits.CM = 0x07; 
+
+    // Sets the PORT A and B    
+    TRISB = 0;; // Sets PORTB as output (RGB LED Control)
+    TRISA0 = 0;  // TRIG pin  (HC-S04 TRIGGER output)
+    TRISA1 = 1;  // ECHO pin  (HC-S04 ECHO input)
+     
+
+    // Checking the RGB LED - Playing with RGB LED before starting the real application.
+    for (int i = 0; i < 6; i++) {
+        set_red_light();
+        __delay_ms(200);
+        set_green_light();
+        __delay_ms(200);
+        set_yellow_light();
+        __delay_ms(200);
+        turn_led_off();
+        __delay_ms(200);
+    } 
+   
+    while (1) {
+        
+        // 
+        TMR1H = 0;  // sets the high byte of the Timer1 counter to 0
+        TMR1L = 0;  // sets the low byte of the Timer1 counter to 0
+
+        RA0 = 1;
+        __delay_us(10);
+        RA0 = 0;
+        while(!RA1); // Wait for ECHO/RA1 pin becomes HIGH
+        TMR1ON = 1;  // Turns TIMER1 on
+        while(RA1);  // Wait for ECHO/RA1 pin becomes LOW
+        TMR1ON = 0;  // Turns TIMER1 off        
+      
+        unsigned int duration = (unsigned int) (TMR1H << 8) | TMR1L;
+        unsigned int distance = duration *0.034/2;
+        
+        if ( distance <= 10) 
+            set_red_light();
+        else if (distance <= 30 )
+            set_yellow_light();
+        else
+            set_green_light();
+    }
+}
