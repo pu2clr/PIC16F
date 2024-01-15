3fce29edecee6548c5ed965cdb81ff05ccb2ed9c
Ricardo Lima Caratti <pu2clr@gmail.com>
2024-01-14 21:51:40 -0300

    Documentation

--- projects/08-LM35_OR_TMP36/MPLAB_EXAMPLES/fever_indicator_pic12f675_lm35.X/main.asm
+++ projects/08-LM35_OR_TMP36/MPLAB_EXAMPLES/fever_indicator_pic12f675_lm35.X/main.asm
@@ -28,39 +28,49 @@ PSECT code, delta=2
 main:
     ; Analog and Digital pins setup
     
-    ;bcf	    STATUS, 5	    ; Selects Bank 0
-    ;clrf    GPIO	    ; Init GPIO	
-    ;bcf	    CMCON, 0	    ; Sets GP0 as output 
-    ;bsf	    STATUS, 5	    ; Selects Bank 1
+    bcf	    STATUS, 5	    ; Selects Bank 0
+    clrf    GPIO	    ; Init GPIO	
+    bcf	    CMCON, 0	    ; Sets GP0 as output 
+    bsf	    STATUS, 5	    ; Selects Bank 1
     
-    ;bsf	    TRISIO, 1	    ; Sets GP1 as input 
-    ;bsf	    ANSEL, 1	    ; Sets GP1 as analog
-    ;movlw   0x01	    ;
-    ;movwf   ADCON0 	    ; Enable ADC
-   
-    ; TO BE CONTINUE....
+    bsf	    TRISIO, 1	    ; Sets GP1 as input 
+    bsf	    ANSEL, 1	    ; Sets GP1 as analog
+    movlw   0x01	    ;
+    movwf   ADCON0 	    ; Enable ADC
 
-    ;
-
-loop:			    ; Endless loop
-    ;
-    ; The temperature reading  process
-    ;
-    ; call AdcRead	    ; read the temperature value
+MainLoopBegin:		    ; Endless loop
+    call AdcRead	    ; read the temperature value
+    ; Considering temperatures of 37 degrees Celsius or higher as fever.
+    movlw 37
+    subwf temp,w
+    btfsc STATUS, 2	    ; Z (Zero Bit)
+    goto  AlmostFever 
+    btfsc STATUS, 2	    ; Z (Zero Bit) 
+    goto  Fever
     
-    ; Assuming the constant is 500 (0x01F4)
-    movlw   LOW(37)	    ; Lower part of the constant
-    subwf   temp, W	    ; Subtract from lower part of the result
-    movlw   HIGH(37)	    ; Higher part of the constant
-    subwf   temp+1, W	    ; Subtract from higher part of the result with borrow
+    
+AlmostFever:		    ; Temperature is 37
+    ; Turn the Yellow LED ON
+    
+    goto MainLoopEnd
+    
+Fever:			    ; Temperature is greater than 37
+    ; Turn the Red LED ON
+    goto MainLoopEnd
+
+Normal: 
+    ; Turn the Gree LED ON
+    
+MainLoopEnd:     
+    
     ; Delay parameters
     call Delay
     
-    goto loop
+    goto MainLoopBegin
      
-    ;
-    ; Your subroutines
-    ;  
+;
+; Your subroutines
+;  
 
 ;
 ; Read the analog value from GP1
@@ -75,26 +85,35 @@ WaitConvertionFinish:		; do while the bit 1 of ADCON0 is 1
     movwf  temp
     movlw  ADRESH
     movwf  temp+1
-    return			; ADRESH << 8 + ADRESL is the combined result of reading   
+    
+    ; Not finished: Need to divide the value by 10
+    
+    return
+
     
 
-; At 4MHz it takes: (5) * 255 * 255 * 3 * 0.000001.
-; It is about 1s (0.975) s    
+; ******************
+; Delay function
+;
+; For an oscillator of 4MHz a regular instructions takes 1us (See pic16f628a Datasheet, page 117).      
+; So, at 4MHz, this Delay subroutine takes about: (5 cycles) * 255 * 255 * 3 * 0.000001 (second)  
+; It is about 1s (0.975 s)  - One second  
 Delay:  
     movlw   255
-    movwf   dummy1
-    movwf   dummy2
-    movlw   3
-    movwf   dummy3
+    movwf   dummy1      ; 255 times
+    movwf   dummy2      ; 255 times (255 * 255)
+    movlw   3			
+    movwf   dummy3      ; 3 times  ( 255 * 255 * 3) 
 DelayLoop:    
-    nop
-    nop
-    decfsz dummy1, f		; dummy1 = dumm1 - 1; if dummy1 = 0 then dummy1 = 255
+    nop                 ; One cycle
+    nop                 ; One cycle
+    decfsz dummy1, f    ; One cycle* (dummy1 = dumm1 - 1) => if dummy1 is 0, after decfsz, it will be 255
+    goto DelayLoop      ; Two cycles
+    decfsz dummy2, f    ; dummy2 = dumm2 - 1; if dummy2 = 0, after decfsz, it will be 255
     goto DelayLoop
-    decfsz dummy2, f		; dummy2 = dumm2 - 1; if dummy2 = 0 then dummy2 = 255
+    decfsz dummy3, f    ; Runs 3 times (255 * 255)		 
     goto DelayLoop
-    decfsz dummy3, f		 
-    goto DelayLoop
+    
     return 
     
 END resetVect
