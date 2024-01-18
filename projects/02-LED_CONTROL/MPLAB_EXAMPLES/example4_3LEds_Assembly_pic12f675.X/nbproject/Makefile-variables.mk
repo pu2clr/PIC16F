ca0c63ad340bd586ec18fc5bebf7537d3a934096
Ricardo Lima Caratti <pu2clr@gmail.com>
2024-01-18 16:00:17 -0300

    Documentation

---
+++ projects/02-LED_CONTROL/MPLAB_EXAMPLES/example4_3LEds_Assembly_pic12f675.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=example4_3LEds_Assembly_pic12f675.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/example4_3LEds_Assembly_pic12f675.X.production.hex
