58c9a0ee6699bd9fb7ea0b0b519ac28c9a7e9b70
Ricardo Lima Caratti <pu2clr@gmail.com>
2024-01-22 12:01:17 -0300

    Doc2

--- projects/02-LED_CONTROL/MPLAB_EXAMPLES/LED_CONTROL_PIC16F628A_2_C.X/nbproject/Makefile-variables.mk
+++ projects/02-LED_CONTROL/MPLAB_EXAMPLES/LED_CONTROL_PIC16F628A_2_C.X/nbproject/Makefile-variables.mk
@@ -6,5 +6,5 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=example2.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/example2.X.production.hex
+CND_ARTIFACT_NAME_default=LED_CONTROL_PIC16F628A_2_C.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/LED_CONTROL_PIC16F628A_2_C.X.production.hex
