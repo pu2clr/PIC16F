db33ea5c4541ece686733222b0d5dbe90fc21344
Ricardo Lima Caratti <pu2clr@gmail.com>
2024-01-30 18:48:41 -0300

    Doc

--- projects/10-74HC595/MPLAB_EXAMPLES/PIC10F200_74HC595_8LEDs_C.X/main.c
+++ projects/10-74HC595/MPLAB_EXAMPLES/PIC10F200_74HC595_8LEDs_C.X/main.c
@@ -12,7 +12,7 @@
 // CONFIG
 #pragma config WDTE = OFF       // Watchdog Timer (WDT disabled)
 #pragma config CP = OFF         // Code Protect (Code protection off)
-#pragma config MCLRE = OFF      // Master Clear Enable (GP3/MCLR pin fuction is digital I/O, MCLR internally tied to VDD)
+#pragma config MCLRE = ON      // Master Clear Enable (GP3/MCLR pin fuction is digital I/O, MCLR internally tied to VDD)
 
 #define _XTAL_FREQ  4000000
 
@@ -20,30 +20,43 @@
 
 void inline doClock() { 
     GP1 = 1; 
-    __delay_ms(100);
+    __delay_us(100);
     GP1 = 0;
-    __delay_ms(100);
+    __delay_us(100);
 }
 
 void inline doEnable() {
     GP2 = 1;
-    __delay_ms(100); 
+    __delay_us(100); 
     GP2 = 0;
 }
 
 void inline sendData(unsigned char data) {
-    do { 
-        GP0 = ( data & 0B00000001);
+    for (unsigned char i = 0; i < 8; i++) { 
+        GP0 = ( data >> i & 0B00000001);
         doClock();
-        data = data >> 1;
-    } while (data);
+    }
     doEnable();
 }
 
 void main(void) {
     TRIS = 0B00000000;          // All GPIO (GP0:GP3) pins as output
-       
-    sendData(0B01010101);
-    while(1);
+    GPIO = 0;
+    
+    sendData(0B11111111);
+    __delay_ms(5000);
+    sendData(0);
+    __delay_ms(5000);   
+    
+    unsigned char data = 1;
+    while(1) {
+        __delay_ms(1000);
+        sendData(data);
+        data = (unsigned char) (data <<  1);
+        if (data == 0) {
+            sendData(0);
+            data = 1;
+        }
+    }
     
 }
