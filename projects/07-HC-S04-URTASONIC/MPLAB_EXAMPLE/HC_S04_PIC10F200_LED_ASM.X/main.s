d2af8762251bb8e7298c09e1b3b64c4cbd6dda27
Ricardo Lima Caratti <pu2clr@gmail.com>
2024-01-29 21:07:41 -0300

    Documentation

---
+++ projects/07-HC-S04-URTASONIC/MPLAB_EXAMPLE/HC_S04_PIC10F200_LED_ASM.X/main.s
@@ -0,0 +1,91 @@
+; HC-S04 with PIC10F200
+; My PIC Journey
+; Author: Ricardo Lima Caratti
+; Jan/2024
+;
+; IMPORTANT: To assemble this code correctly, please follow the steps below:
+; 1. Go to "Project Properties" in MPLAB X.
+; 2. Select "Global Options" for the pic-as assembler/compiler.
+; 3. In the "Additional Options" box, enter the following parameters:
+; -Wl,-pBlinkCode=0h
+;
+; Please check the BlinkCode reference in the "PSECT" directive below.
+;
+; You will find good tips about the PIC10F200 here:
+; https://www.circuitbread.com/tutorials/christmas-lights-special-microcontroller-basics-pic10f200
+
+ 
+    
+#include <xc.inc>
+
+; CONFIG
+  CONFIG  WDTE = OFF           ; Watchdog Timer (WDT disabled)
+  CONFIG  CP = OFF             ; Code Protect (Code protection off)
+  CONFIG  MCLRE = ON	       ; Master Clear Enable (GP3/MCLR pin function  is MCLR)
+
+  
+; Declare your variables here
+
+dummy1 equ 0x10
+; dummy2 equ 0x11
+; dummy3 equ 0x12 
+ 
+PSECT BlinkCode, class=CODE, delta=2
+
+MAIN:
+    
+    ; TIMER0 AND PRESCALER SETUP
+    ; TOCS = 0 => INTERNAL INSTRUCTION CYCLE; 
+    ; PSA = 0 => TIMER0; and
+    ; PRESCALER (PS2,PS1 AND PS0 => 1:128)
+    movlw  0B11010110	    ;   
+    option
+    ; GPIO SETUP
+    ; GP0 -> LED/output; 
+    ; GP1 -> Trigger/output; and 
+    ; GP2 = Echo/input 
+    movlw   0B00000100
+    tris    GPIO
+MainLoop:		    ; Endless loop
+    bsf	    GPIO,1	    ; Send 10us signal via GP1
+    call    Delay10us
+    bcf	    GPIO,1
+    
+
+    goto    MainLoop
+     
+; ******************
+; Delay function
+
+; At 4 MHz, one instruction takes 1?s
+; So, this soubroutine should take about 10?s 
+; This time is used by the HC-S04 ultrasonic sensor 
+; to determine the distance. 	
+Delay10us:
+    nop		; 8 cycle
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop	    
+    retlw 0	; 2 cycles    
+
+; It takes about 2ms
+Delay2ms: 
+    movlw  200
+    movwf  dummy1
+LoopDelay2ms: 
+    call Delay10us    
+    decfsz dummy1, f
+    goto LoopDelay2ms
+    retlw   0
+    
+END MAIN
+
+
+
+
+
+
