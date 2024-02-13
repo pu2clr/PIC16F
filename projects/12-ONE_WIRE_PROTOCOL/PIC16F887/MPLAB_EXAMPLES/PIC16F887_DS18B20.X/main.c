5d16821ae5165203afcefc4c5cdf28304ec09e21
Ricardo Lima Caratti <pu2clr@gmail.com>
2024-02-13 15:39:04 -0300

    Doc

---
+++ projects/12-ONE_WIRE_PROTOCOL/PIC16F887/MPLAB_EXAMPLES/PIC16F887_DS18B20.X/main.c
@@ -0,0 +1,17 @@
+/*
+ * File:   main.c
+ * Author: rcaratti
+ *
+ * Created on February 13, 2024, 1:21 PM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    
+    while (1) {
+        
+    }
+    return;
+}
