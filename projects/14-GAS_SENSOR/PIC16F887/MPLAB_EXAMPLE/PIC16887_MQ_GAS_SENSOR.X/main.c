5c1068c0f6881c33a2ef49259806fd4a9cf6267c
Ricardo Lima Caratti <pu2clr@gmail.com>
2024-02-27 10:31:25 -0300

    Doc

---
+++ projects/14-GAS_SENSOR/PIC16F887/MPLAB_EXAMPLE/PIC16887_MQ_GAS_SENSOR.X/main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main.c
+ * Author: rcaratti
+ *
+ * Created on February 27, 2024, 10:18 AM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ projects/14-GAS_SENSOR/PIC16F887/MPLAB_EXAMPLE/PIC16887_MQ_GAS_SENSOR.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=PIC16887_MQ_GAS_SENSOR.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/PIC16887_MQ_GAS_SENSOR.X.production.hex
