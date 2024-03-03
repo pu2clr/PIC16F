564893a7484090ae2d00e92312af3338921cc0e9
Ricardo Lima Caratti <pu2clr@gmail.com>
2024-03-03 11:45:15 -0300

    Doc

---
+++ projects/99-Servomotor/PIC16F887/MPLAB_EXAMPLES/pic16f887_example.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=pic16f887_example.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/pic16f887_example.X.production.hex
