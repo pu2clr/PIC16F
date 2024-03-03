9bcd24afdc64707905f7036021ebf48183971853
Ricardo Lima Caratti <pu2clr@gmail.com>
2024-03-03 20:50:02 -0300

    Doc

---
+++ projects/99-Servomotor/PIC10F200/MPLAB_EXAMPLES/tmp/tmp.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=cb_p10_electr_lock.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/cb_p10_electr_lock.X.production.hex
