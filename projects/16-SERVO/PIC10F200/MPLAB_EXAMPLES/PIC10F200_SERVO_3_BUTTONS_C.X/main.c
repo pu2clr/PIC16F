cda48c61afd557a6dedd713bb456c45307a84471
Ricardo Lima Caratti <pu2clr@gmail.com>
2024-03-06 16:55:49 -0300

    Doc

--- projects/16-SERVO/PIC10F200/MPLAB_EXAMPLES/PIC10F200_SERVO_3_BUTTONS_C.X/main.c
+++ projects/16-SERVO/PIC10F200/MPLAB_EXAMPLES/PIC10F200_SERVO_3_BUTTONS_C.X/main.c
@@ -19,6 +19,7 @@
 void inline delayMS(uint8_t param) {
     for (uint8_t i = 0; i < param; i++) {
         for (uint8_t j = 0; j < 200; j++) { 
+            asm("nop");
         }
     }
 }
@@ -46,15 +47,17 @@ void main(void) {
     OPTION = 0B10011111; 
     TRISGPIO = 0B00001011;      // GP0, GP1 and GP3 as input and GP2 as output
 
-    
-
     RotateServo(3);
     
     while (1) {
-        if (GP3 == 0) RotateServo(3);
-        if (GP1 == 0) RotateServo(4);
-        if (GP0 == 0) RotateServo(2);
-        delayMS(255);
+        if (GP3 == 0) 
+            RotateServo(3);
+        else if (GP1 == 0) 
+            RotateServo(4);
+        else if (GP0 == 0) 
+            RotateServo(2);
+        
+        delayMS(40);
     }
 
 }
