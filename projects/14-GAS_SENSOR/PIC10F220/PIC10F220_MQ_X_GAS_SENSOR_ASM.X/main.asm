e3c9b53c6ebbeef95d15da7fc9773faf79c479e4
Ricardo Lima Caratti <pu2clr@gmail.com>
2024-03-07 13:07:38 -0300

    Doc

--- projects/14-GAS_SENSOR/PIC10F220/PIC10F220_MQ_X_GAS_SENSOR_ASM.X/main.asm
+++ projects/14-GAS_SENSOR/PIC10F220/PIC10F220_MQ_X_GAS_SENSOR_ASM.X/main.asm
@@ -21,7 +21,7 @@
 ; CONFIG
   CONFIG  WDTE = OFF           ; Watchdog Timer (WDT disabled)
   CONFIG  CP = OFF             ; Code Protect (Code protection off)
-  CONFIG  MCLRE = OFF	       ; Master Clear Enable (GP3/MCLR pin function  is MCLR)
+  CONFIG  MCLRE = ON	       ; Master Clear Enable (GP3/MCLR pin function  is MCLR)
 
 ; Declare your variables here
 
