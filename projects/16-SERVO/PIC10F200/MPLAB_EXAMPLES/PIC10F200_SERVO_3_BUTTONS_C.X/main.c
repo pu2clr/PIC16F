d19df1368d6e0160b8a2f29b76cd6af1ee93eda4
Ricardo Lima Caratti <pu2clr@gmail.com>
2024-03-08 18:32:34 -0300

    Doc

--- projects/16-SERVO/PIC10F200/MPLAB_EXAMPLES/PIC10F200_SERVO_3_BUTTONS_C.X/main.c
+++ projects/16-SERVO/PIC10F200/MPLAB_EXAMPLES/PIC10F200_SERVO_3_BUTTONS_C.X/main.c
@@ -12,13 +12,11 @@
 #pragma config CP = OFF         // Code Protect (Code protection off)
 #pragma config MCLRE = OFF      // Master Clear Enable (GP3/MCLR pin fuction is digital I/O, MCLR internally tied to VDD)
 
-#define _XTAL_FREQ  4000000
-
 
 // it takes about param x 255 us
 void inline delayMS(uint8_t param) {
     for (uint8_t i = 0; i < param; i++) {
-        for (uint8_t j = 0; j < 200; j++) { 
+        for (uint8_t j = 0; j < 100; j++) { 
             asm("nop");
         }
     }
@@ -31,23 +29,22 @@ void RotateServo(uint8_t duration) {
         GP2 = 1;
         delayMS(duration);
         GP2 = 0;
-        delayMS(25);
+        delayMS(22);
     } while (i--);
 }
 
 void main(void) {
     
-    // OPTION register setup 
-    // Prescaler Rate: 256
-    // Prescaler assigned to the WDT
-    // Increment on high-to-low transition on the T0CKI pin
-    // Transition on internal instruction cycle clock, FOSC/4
-    // GPPU disabled 
-    // GPWU disabled
-    OPTION = 0B10011111; 
+
+    OPTION = 0B10010000; 
     TRIS = 0B00001011;      // GP0, GP1 and GP3 as input and GP2 as output
 
     RotateServo(3);
+    delayMS(255);
+    delayMS(255);
+    delayMS(255);
+    delayMS(255);
+    RotateServo(2);
     
     while (1) {
         if (GP3 == 0) 
