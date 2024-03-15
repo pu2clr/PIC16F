90e74c1df12da4bc37eaf2ab6635994f6d40d84a
Ricardo Lima Caratti <pu2clr@gmail.com>
2024-03-15 16:45:09 -0300

    Doc

---
+++ projects/17-INFRARED_IR_TX_RX/PIC10F200/MPLAB_EXAMPLE/PIC10F200_IR_RX_REPLICATE_TX_ASM.X/main.asm
@@ -0,0 +1,48 @@
+; UNDER CONSTRUCTION...
+; Infrared receiver    RX RX RX RX RX RX 
+; IMPORTANT: If you are using the PIC10F200, to assemble this code correctly, please follow the steps below:
+; 1. Go to "Project Properties" in MPLAB X.
+; 2. Select "Global Options" for the pic-as assembler/compiler.
+; 3. In the "Additional Options" box, enter the following parameters:
+; -Wl,-pAsmCode=0h
+    
+#include <xc.inc>
+ 
+ 
+    
+; CONFIG
+  CONFIG  WDTE = OFF           ; Watchdog Timer (WDT disabled)
+  CONFIG  CP = OFF             ; Code Protect (Code protection off)
+  CONFIG  MCLRE = OFF	         
+  
+; Declare your variables here  
+  
+PSECT AsmCode, class=CODE, delta=2
+
+MAIN:
+    ; GPIO and registers setup
+    clrf   GPIO		    ; Sets all GPIO pins as output
+    movlw  0B00000001	    ; Set GP0 as input and GP1 as output
+    TRIS   GPIO
+    
+MainLoop:		    ; Endless loop
+    ; While GP0 is 0
+    btfsc   GPIO, 0
+    goto    LEDOn 
+    bcf	    GPIO, 1	    ; Turn the LED OFF
+    goto    MainLoop
+LEDOn:
+    bsf	    GPIO, 1	    ; Turn the LED ON
+    goto    MainLoop
+
+    
+END MAIN
+
+
+
+
+
+
+
+
+
