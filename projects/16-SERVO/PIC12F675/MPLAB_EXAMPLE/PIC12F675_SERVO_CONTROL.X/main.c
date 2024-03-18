7944715a04f1c36aba5fb006804411d97c4abeaa
Ricardo Lima Caratti <pu2clr@gmail.com>
2024-03-17 21:43:40 -0300

    Doc

--- projects/16-SERVO/PIC12F675/MPLAB_EXAMPLE/PIC12F675_SERVO_CONTROL.X/main.c
+++ projects/16-SERVO/PIC12F675/MPLAB_EXAMPLE/PIC12F675_SERVO_CONTROL.X/main.c
@@ -12,7 +12,7 @@
 #pragma config FOSC = INTRCIO   // Oscillator Selection bits (INTOSC oscillator: I/O function on GP4/OSC2/CLKOUT pin, I/O function on GP5/OSC1/CLKIN)
 #pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
 #pragma config PWRTE = OFF      // Power-Up Timer Enable bit (PWRT disabled)
-#pragma config MCLRE = ON       // GP3/MCLR pin function select (GP3/MCLR pin function is MCLR)
+#pragma config MCLRE = OFF       // GP3/MCLR pin function select (GP3/MCLR pin function is MCLR)
 #pragma config BOREN = ON       // Brown-out Detect Enable bit (BOD enabled)
 #pragma config CP = OFF         // Code Protection bit (Program Memory code protection is disabled)
 #pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
