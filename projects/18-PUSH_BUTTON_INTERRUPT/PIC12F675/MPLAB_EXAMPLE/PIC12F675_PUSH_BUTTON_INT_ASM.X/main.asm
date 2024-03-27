eace2d96ccf9d1e8194e7245deddcd5dcfe90379
Ricardo Lima Caratti <pu2clr@gmail.com>
2024-03-26 22:19:11 -0300

    DOC

---
+++ projects/18-PUSH_BUTTON_INTERRUPT/PIC12F675/MPLAB_EXAMPLE/PIC12F675_PUSH_BUTTON_INT_ASM.X/main.asm
@@ -0,0 +1,128 @@
+; UNDER  IMPROVEMENTS...
+; I couldn't find clear documentation on how to configure the interrupt service using "pic-as". 
+; Therefore, I tried some configurations so that the occurrence of a desired interrupt would 
+; divert the program flow to address 4h. This was possible by adding special parameters as shown below.
+; Go to properties and set pic-as Additiontal Options: -Wl,-PresetVec=0x0,-PisrVec=0x04   
+;    
+; Author: Ricardo Lima Caratti
+; Jan/2024
+    
+#include <xc.inc>
+ 
+; CONFIG
+  CONFIG  FOSC = INTRCIO        ; Oscillator Selection bits (INTOSC oscillator: I/O function on GP4/OSC2/CLKOUT pin, I/O function on GP5/OSC1/CLKIN)
+  CONFIG  WDTE = OFF            ; Watchdog Timer Enable bit (WDT disabled)
+  CONFIG  PWRTE = OFF           ; Power-Up Timer Enable bit (PWRT disabled)
+  CONFIG  MCLRE = ON            ; GP3/MCLR pin function select (GP3/MCLR pin function is MCLR)
+  CONFIG  BOREN = ON            ; Brown-out Detect Enable bit (BOD enabled)
+  CONFIG  CP = OFF              ; Code Protection bit (Program Memory code protection is disabled)
+  CONFIG  CPD = OFF             ; Data Code Protection bit (Data memory code protection is disabled) 
+  
+; declare your variables here
+
+adcValueL   equ 0x20		; 8 bits less significant value of the adc
+adcValueH   equ 0x21		; 8 bits most significant value of the adc
+delayParam  equ	0x22  
+ind_I	    equ	0x23
+ind_J	    equ 0x24	    
+   	    
+PSECT resetVec, class=CODE, delta=2 
+ORG 0x0000	    
+resetVect:
+    PAGESEL main
+    goto main
+;
+; INTERRUPT IMPLEMENTATION 
+; THIS FUNCTION WILL BE CALLED EVERY TMR0 Overflow
+; pic-as Additiontal Options: -Wl,-PresetVec=0x0,-PisrVec=0x04    
+PSECT isrVec, class=CODE, delta=2
+ORG 0x0004     
+isrVec:  
+    PAGESEL interrupt
+    goto interrupt
+  
+interrupt: 
+   
+    bcf	    STATUS, 5
+    
+    bcf	    INTCON, 7	; Disables GIE
+    
+    ; check if the interrupt was trigged by Timer0	
+    btfss   INTCON, 1		; INTCON - INTF: GP2/INT External Interrupt Flag bit
+    goto    INT_FINISH
+    btfss   GPIO, 5		; Toggle LED (ON/OFF)
+    goto    INT_SWITCH_ON
+    goto    INT_SWITCH_OFF
+INT_SWITCH_ON:    
+    bcf	    GPIO,5
+    goto    INT_CONTINUE
+INT_SWITCH_OFF:    
+    bsf	    GPIO,5
+INT_CONTINUE:    
+    movlw   1
+    call    Delay
+    bcf	    INTCON, 1  
+INT_FINISH:
+    bsf	    INTCON, 7		; Enables GIE
+   
+    retfie    
+      
+; PSECT code, delta=2
+main: 
+    ; Bank 1
+    bsf	    STATUS,5	    ; Selects Bank 1  
+    movlw   0B00000100	    ; GP1 as input and GP1, GP2, GP4 and GP5 as digital output
+    movwf   TRISIO	    ; Sets all GPIO as output    
+    clrf    ANSEL	    ; Disable Analog setup   
+
+    ; OPTION_REG setup
+    ; bit 5 = 0 -> Internal instruction cycle clock;
+    movlw   0B01000000	
+    movwf   OPTION_REG	    
+    ; Bank 0
+    bcf	    STATUS,5 
+    clrf    GPIO	; Turn all GPIO pins low
+    
+    ; INTCON setup
+    ; bit 7 (GIE) = 1 => Enables all unmasked interrupts
+    ; bit 5 (T0IE) =  1 => Enables the TMR0 interrupt
+    movlw   0B11000000
+    iorwf   INTCON
+       
+MainLoopBegin:		; Endless loop
+    sleep      
+    goto    MainLoopBegin
+     
+
+; ******************
+; Delay function
+;
+
+Delay:  
+    movwf   delayParam
+    movlw   100
+    movwf   ind_I	; 100 times
+    movwf   ind_J	; 100 times 
+			; 100 * 100 * delayParam    
+DelayLoop:    
+    nop                 ; One cycle
+    nop                 ; One cycle
+    decfsz ind_I, f	; One cycle * (ind_I = ind_I - 1) => if ind_I is 0, after decfsz, it will be 255
+    goto DelayLoop      ; Two cycles
+    decfsz ind_J, f	; ind_J = ind_J - 1; if ind_J = 0, after decfsz, it will be 255
+    goto DelayLoop
+    decfsz delayParam,f ; Runs WREG/delayParam * (100 * 100)		 
+    goto DelayLoop
+    
+    return     
+    
+    
+END resetVect
+
+
+
+
+
+
+
+
---
+++ projects/18-PUSH_BUTTON_INTERRUPT/PIC16F628A/MPLAB_EXAMPLE/PIC16F628A_PUSH_BUTTON_INT_ASM.X/main.s
@@ -0,0 +1,62 @@
+; UNDER CONSTRUCTION...
+; Assembly source 
+; Author: Ricardo Lima Caratti - Jan/2024
+
+#include <xc.inc>
+    
+; CONFIG
+  CONFIG  FOSC = INTOSCIO       ; Oscillator Selection bits (INTOSC oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
+  CONFIG  WDTE = OFF            ; Watchdog Timer disable bit 
+  CONFIG  PWRTE = OFF           ; Power-up Timer Enable bit (PWRT disabled)
+  CONFIG  MCLRE = ON            ; RA5/MCLR/VPP Pin Function Select bit (RA5/MCLR/VPP pin function is MCLR)
+  CONFIG  BOREN = ON            ; Brown-out Detect Enable bit (BOD enabled)
+  CONFIG  LVP = OFF             ; Low-Voltage Programming disble
+  CONFIG  CPD = OFF             ; Data EE Memory Code Protection bit (Data memory code protection off)
+  CONFIG  CP = OFF              ; Flash Program Memory Code Protection bit (Code protection off)
+
+// config statements should precede project file includes.
+
+ind_I		equ 0x20
+ind_J		equ 0x21
+delayParam	equ 0x22 
+  
+PSECT resetVector, class=CODE, delta=2
+resetVect:
+    PAGESEL main
+    goto main
+PSECT code, delta=2
+main:
+ 
+loop:			; Loop without a stopping condition - here is your application code
+
+    goto loop
+
+
+; ******************
+; Delay function
+;
+; For an oscillator of 4MHz a regular instructions takes 1us (See pic16f628a Datasheet, page 117).      
+; So, at 4MHz, this Delay subroutine takes about: (5 cycles) * 255 * 255 * 3 * 0.000001 (second)  
+; It is about 1s (0.975 s)    
+Delay:  
+    movwf   delayParam    
+    movlw   100
+    movwf   ind_I
+    movwf   ind_J
+DelayLoop:    
+    nop
+    nop
+    decfsz ind_I, f		
+    goto DelayLoop
+    decfsz ind_J, f	
+    goto DelayLoop
+    decfsz delayParam, f		 
+    goto DelayLoop
+    return 
+    
+END resetVect
+    
+
+
+
+
