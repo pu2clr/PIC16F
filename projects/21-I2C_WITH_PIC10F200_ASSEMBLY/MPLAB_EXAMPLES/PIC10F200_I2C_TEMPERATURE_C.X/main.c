bffeb1f6413d00a1c917466163ae0943593741c8
Ricardo Lima Caratti <pu2clr@gmail.com>
2024-04-16 14:45:07 -0300

    Doc

--- projects/21-I2C_WITH_PIC10F200_ASSEMBLY/MPLAB_EXAMPLES/PIC10F200_I2C_TEMPERATURE_C.X/main.c
+++ projects/21-I2C_WITH_PIC10F200_ASSEMBLY/MPLAB_EXAMPLES/PIC10F200_I2C_TEMPERATURE_C.X/main.c
@@ -1,94 +1,173 @@
 /*
- * File:   main.c
- * Author: rcaratti
- *
+ * UNDER CONSTRUCTION...
+ * I2C protocol implementation
+ * 
  * Created on April 15, 2024, 12:04 PM
+ * Ricardo Lima Caratti
+ * References: https://www.ti.com/lit/an/slva704/slva704.pdf
  */
 
 #include <xc.h>
 
+// Sets the SDA and SCL pins of PIC10F200
+// Change this if you have another setup. 
+#define SDA_PIN     GP2         // Data  pin
+#define SCL_PIN     GP1         // Clock pin
 
-#define SDA_PIN     GP2
-#define SCL_PIN     GP1
 
 
-void i2cSetSdaLow() {
-    SDA_PIN = 0;        
+
+void i2cSendAck() {
+    SDA_PIN = 0;    // Sets SDA PIN as output
     TRIS = GPIO;
+    
+    SCL_PIN = 0;
+    SDA_PIN = 0;
+
+    SCL_PIN = 1;
+    SCL_PIN = 0;
+    
 }
 
-void i2cSetSdaHigh() {
+void i2cSendNAck() {
+    SDA_PIN = 0;    // Sets SDA PIN as output
+    TRIS = GPIO;
+    
+    
+    SCL_PIN = 0;
     SDA_PIN = 1;
-    TRIS = GPIO;
+
+    SCL_PIN = 1;
+    SCL_PIN = 0;
 }
 
-void i2cSetSclLow() {
+/**
+ * @brief Gets Acknowledge (ACK)
+ * @return ACK value (the current SDA value
+ */
+unsigned char i2cReceiveAck() {
+    uint8_t ack;
+
+    SDA_PIN = 1;    // Sets SDA PIN as input
+    TRIS = GPIO;
+        
+    SCL_PIN = 1;
+    ack = SDA_PIN;
+    SCL_PIN = 0; 
+    
+    return ack;
+}
+
+/**
+ * Starts transaction 
+ */
+void i2cBeginTransaction() {
+    SDA_PIN = 0;                // Sets SDA and SCL pins as OUTPUT
     SCL_PIN = 0;
     TRIS = GPIO;
-} 
-
-void i2cSetSclHigh() {
+    
+    SDA_PIN = 1;
     SCL_PIN = 1;
+ 
+    SDA_PIN = 0;
+    SCL_PIN = 0;    
+    
+    SDA_PIN = 1;   
+}
+
+
+void i2cEndTransaction() {
+    SDA_PIN = 0;                // Sets SDA and SCL pins as OUTPUT
+    SCL_PIN = 0;
     TRIS = GPIO;
-}
 
-void i2cBegin() {
-    i2cSetSclHigh();
-    i2cSetSdaLow();
-}
-
-void i2cStop() {
-    i2cSetSdaLow(); 
-    i2cSetSclHigh();
-    i2cSetSdaHigh();
-}
-
-void i2cSendAKC() {
-    i2cSetSdaLow();
-    i2cSetSclHigh();
-    i2cSetSclLow();
-    i2cSetSdaHigh();
-}
-
-void i2cSendNAKC() {
-    i2cSetSdaHigh(); 
-    i2cSetSclHigh();
-    i2cSetSclLow();
+    SDA_PIN = 0;
+    SCL_PIN = 1;
+    SDA_PIN = 1;
 }
 
 
-void writeByte(unsigned char value) { 
+
+
+void i2cWriteByte(unsigned char value) { 
    uint8_t aux; 
+   
+   SDA_PIN = 0;         // Sets SDA PIN as output
+   TRIS = GPIO;
+   
    for (uint8_t i = 0; i < 8; i++) {
        aux = 0B10000000 & value;
        if ( aux ) {
-          i2cSetSdaHigh();          // writes 1
+          SDA_PIN = 1;
        } else {
-          i2cSetSdaLow();           // write  0
+           SDA_PIN = 0;
        }
-       i2cSetSclHigh();
+       SCL_PIN = 1;
+       SCL_PIN = 0;
        value = (uint8_t) (value << 1);          // prepare the next bit to be written 
    } 
-   // To be continue
 }
 
-unsigned char readByte() { 
+unsigned char i2cReadByte() { 
     uint8_t value = 0;
+    
+    SDA_PIN = 1;         // Sets SDA PIN as input
+    TRIS = GPIO;
+       
     for (uint8_t i = 0; i < 8; i++) {
+        SCL_PIN = 1;
         value = (uint8_t) (value << 1);
-        i2cSetSclLow();
-        i2cSetSclHigh();
-        if ( SDA_PIN == 0) value = value & 0B11111110;
+        if ( SDA_PIN ) value = value |  0B00000001;
+        SCL_PIN = 0;
     }
-    i2cSetSclLow();
     return value;
+}
+
+void i2cSendCommand(uint8_t deviceAddress, uint8_t value ) {
+    
+    i2cBeginTransaction();
+    i2cWriteByte(deviceAddress);
+    i2cReceiveAck();
+    i2cWriteByte(value);
+    i2cReceiveAck();
+    i2cEndTransaction();
+
+}
+
+uint8_t i2cGetData(uint8_t deviceAddress) {
+    
+    uint8_t data;
+    
+    i2cBeginTransaction();
+    
+    i2cWriteByte(deviceAddress);
+    i2cReceiveAck();
+    
+    data = i2cReadByte();
+    i2cSendAck();
+    i2cSendNAck();
+    
+    i2cEndTransaction();
+    
+    return data;
 }
 
 
 void main(void) {
-    i2cBegin();
-    i2cStop();
-    writeByte(0x0F);
-    readByte();
+    // GPWU: Disable (Enable Wake-up on Pin Change bit)
+    // GPPU: Enable (Enable Weak Pull-ups bit)
+    // T0CS: Transition on internal instruction cycle clock, FOSC/4
+    // T0SE: Enable (Timer0 Source Edge Select bit)
+    // PSA.: 0 = Prescaler assigned to Timer0
+    // PS<2:0>: Prescaler Rate Select bits (1:256)
+    OPTION = 0B10010111;
+    TRIS = 0B0000111;        
+
+    while (1) {
+        i2cSendCommand(0x0F, 1);
+        uint8_t value = i2cGetData(0x0F);
+        i2cSendCommand(0x0F, value);
+    }
+    
     return;
 }
